f1165a00263d40d06fc08a4ceb3cf2d09c6cca2e
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-26 16:40:09 -0600

    dropdown logic added grab the value of dropdown and change background image to value then display stats.

--- app.js
+++ app.js
@@ -15,41 +15,42 @@ const catchphraseButton = document.getElementById('catchphrase-button');
 const resetcatchPhrase = document.getElementById('reset-catchPhrase');
 
 // set state for how many times the user changes the head, middle, and bottom
-let head = 0;
-let middle = 0;
-let bottom = 0;
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
 // set state for all of the character's catchphrases
-let catchPhrases = [];
+const catchPhrases = [];
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-    let userHead = document.getElementById('head-dropdown').value;
+    let headValue = headDropdown.value;
     // increment the head change count state
-    head++;
+    headCount++;
     // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    headEl.style.backgroundImage = `url("./assets/${headValue}-head.png")`;
     // update the stats to show the new count (refactor to/call displayStats() to do this work)
+    displayStats();
 });
 
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
-    let userMiddle = document.getElementById('middle-dropdown').value;
+    let middleValue = document.getElementById('middle-dropdown').value;
     // increment the middle change count state
-    middle++;
+    middleCount++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleEl.style.backgroundImage = `url("./assets/${middleValue}-middle.png")`;
     // update the stats to show the new count (refactor to/call displayStats() to do this work)
 });
 
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
-    let userBottom = document.getElementById('bottom-dropdown').value;
+    let bottomValue = document.getElementById('bottom-dropdown').value;
     // increment the bottom change count state
-    bottom++;
+    bottomCount++;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    bottomEl.style.backgroundImage = `url("./assets/${bottomValue}-bottom.png")`;
     // update the stats to show the new count (refactor to/call displayStats() to do this work)
 });
 
